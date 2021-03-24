999332e64ec9d87193ffe1db0909588eed82143d
SilvanXY <silvan.rebmann@gmail.com>
2021-03-24 14:41:51 +0100

    Erstellen des Interfaces

---
+++ Projektarbeit M226/src/m226/InterfaceBooks.java
@@ -0,0 +1,13 @@
+package m226;
+
+public interface InterfaceBooks{
+	
+	public void ausleihen();
+	
+	public void zurückgeben();
+	
+	public void buchErstellen();
+	
+	public void buchLöschen();
+
+}
