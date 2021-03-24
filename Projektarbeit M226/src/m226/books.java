f2e9b0ec11c677050c687d210f3e23b7a8822dd5
bezzgi <salathe.beat@bluewin.ch>
2021-03-24 13:36:17 +0100

    get & set

--- Projektarbeit M226/src/m226/books.java
+++ Projektarbeit M226/src/m226/books.java
@@ -7,5 +7,29 @@ public class books {
 	int pages;
 	boolean lent;
 	
-	
+	//Setter und Getter für alle Variablen
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setLent(boolean lent) {
+		this.lent = lent;
+	}
+	public Boolean getLent() {
+		return lent;
+	}
 }
