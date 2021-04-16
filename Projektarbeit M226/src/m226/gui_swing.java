3f4c57e69d57cd340cff6476a47e6a9157bd2275
bezzgi <salathe.beat@bluewin.ch>
2021-04-16 08:34:47 +0200

    kleine bug fixes

--- Projektarbeit M226/src/m226/gui_swing.java
+++ Projektarbeit M226/src/m226/gui_swing.java
@@ -3,6 +3,7 @@ package m226;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -70,7 +71,7 @@ public class gui_swing  extends JFrame implements ActionListener{
 		 
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1100, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNeuesBuch = new JLabel("Neues Buch");
